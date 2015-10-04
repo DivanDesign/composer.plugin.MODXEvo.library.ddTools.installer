a62bc1d20d1ce0773c348eb8b7b82f6838acac31
Pavel Unzhakov <phm.psthmn@gmail.com>
2015-10-04 18:39:38 +0500

    Version 1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# ddTools installer
+
+A composer plugin allowing installation of ddTools maintaining compatibility with the old versions of the library.
+___
+Visit the following [link](http://code.divandesign.biz/modx/ddtools) to read the documentation, instructions & changelog.
---
+++ src/Installer.php
@@ -0,0 +1,88 @@
+<?php
+namespace MODXEvoDDToolsInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+use Composer\Repository\InstalledRepositoryInterface;
+
+class Installer extends LibraryInstaller {
+	private $ddToolsClassFileName = "modx.ddtools.class.php",
+			$ddToolsOldVersionBackupFileName = "modx.ddtools.class_old.php",
+			$ddToolsPath = 'assets/libs/ddTools/',
+			$ddToolsDeprecatedPath = "assets/snippets/ddTools/",
+			$ddToolsPackageName = "dd/modxevo-ddtools",
+			$ddToolsType = "modxevo-library-ddtools";
+	
+	public function getPackageBasePath(PackageInterface $package){
+		if($package->getPrettyName() !== $this->ddToolsPackageName){
+			throw new \InvalidArgumentException("The library being installed ({$package->getPrettyName()}) is not ddTools ({$this->ddToolsPackageName}). Installation aborted.");
+		}
+		
+		return $this->ddToolsPath;
+	}
+	
+	public function supports($packageType){
+		return $this->ddToolsType === $packageType;
+	}
+	
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package){
+		parent::install($repo, $package);
+		$this->overrideOldVersionFile();
+	}
+	
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target){
+		parent::update($repo, $initial, $target);
+		$this->overrideOldVersionFile();
+	}
+	
+	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package){
+		$this->restoreOldVersionFile();
+		parent::uninstall($repo, $package);
+	}
+	
+	/**
+	 * overrideOldVersionFile
+	 * 
+	 * Overrides an old version of ddTools if exists.
+	 */
+	private function overrideOldVersionFile(){
+		$oldVersionFilePath = $this->ddToolsDeprecatedPath.$this->ddToolsClassFileName;
+		
+		//Check if an old version of the library is in assets/snippets
+		if(is_file($oldVersionFilePath)){
+			//Backup of the old version
+			file_put_contents(
+				$this->ddToolsDeprecatedPath.$this->ddToolsOldVersionBackupFileName,
+				file_get_contents($oldVersionFilePath)
+			);
+			
+			//Override the old version with a reference to the version being installed
+			file_put_contents(
+				$this->ddToolsDeprecatedPath.$this->ddToolsClassFileName,
+				"<?php require_once('".realpath($this->ddToolsPath.$this->ddToolsClassFileName)."'); ?>"
+			);
+		}
+	}
+	
+	/**
+	 * restoreOldVersionFile
+	 * 
+	 * Restores the old version of ddTools if existed.
+	 */
+	private function restoreOldVersionFile(){
+		$oldVersionFilePath = $this->ddToolsDeprecatedPath.$this->ddToolsClassFileName;
+		$oldVersionBackupFilePath = $this->ddToolsDeprecatedPath.$this->ddToolsOldVersionBackupFileName;
+		
+		//Check if an old version of the library is in assets/snippets and its backup exists 
+		if(is_file($oldVersionBackupFilePath) && is_file($oldVersionFilePath)){
+			//Restore from the backup
+			file_put_contents(
+				$this->ddToolsDeprecatedPath.$this->ddToolsClassFileName,
+				file_get_contents($this->ddToolsDeprecatedPath.$this->ddToolsOldVersionBackupFileName)
+			);
+			
+			//Remove backup
+			unlink($oldVersionBackupFilePath);
+		}
+	}
+}
